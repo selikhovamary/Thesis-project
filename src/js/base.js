60e369653c61cf8a05622dcc88dfcc3362619516
selikhovamary <maryiaselikhova@gmail.com>
2019-11-09 18:38:20 +0300

    js in category section

--- src/js/base.js
+++ src/js/base.js
@@ -1,8 +1,35 @@
 var navbar = document.querySelector('.headerItems')
+var navbarUl = document.querySelector('.headerNavbar');
+var categories = document.querySelector('.photoCategories')
+var sectionPic = document.querySelectorAll('.section__pic')
+
+
 window.addEventListener('scroll', function(){
 if (pageYOffset >= 10) {
-    navbar.classList.add('bg')
+    navbar.classList.add('bg','border');
+    navbarUl.classList.remove('border')
 } else if (navbar.classList.contains('bg')) {
-    navbar.classList.remove('bg')
+    navbar.classList.remove('bg', 'border');
+    navbarUl.classList.add('border')
+
 }
 })
+
+for (i = 0; i < sectionPic.length; i++){
+sectionPic[i].addEventListener('mouseover', function(){
+var pic = this.querySelector('.gallery__pic')
+pic.classList.add('opacityImg')  
+var link = this.querySelector('.categoryLink');
+link.classList.remove('hidden');
+link.classList.remove('opacity');
+
+ });
+ sectionPic[i].addEventListener('mouseout', function(){
+    var pic = this.querySelector('.gallery__pic')
+pic.classList.remove('opacityImg')     
+var link = this.querySelector('.categoryLink');
+link.classList.add('opacity');
+
+
+})
+}

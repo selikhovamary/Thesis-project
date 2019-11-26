52b1da665c5c54af368937d681d496681e084398
selikhovamary <maryiaselikhova@gmail.com>
2019-11-27 00:33:26 +0300

    improved bot

--- src/js/bot.js
+++ src/js/bot.js
@@ -3,17 +3,42 @@ var firstName = document.querySelector('#name');
 var mobileTel = document.querySelector('#tel');
 var msg;
 
-sendMsg.addEventListener('click', function(){
-    if (firstName.value && mobileTel.value){
-msg = 'Name: ' + firstName.value + ' ' + '; Tel: ' + mobileTel.value;
-var botLink = 'https://api.telegram.org/bot892587873:AAFw-auKXmBAvQ0x77e-hty-BvS-faOHS0Y/sendMessage?chat_id=214831481&text=' + msg;
-fetch(botLink)} 
+sendMsg.addEventListener('click', function () {
+    deleteError()
+    if (firstName.value && mobileTel.value) {
+        msg = 'Name: ' + firstName.value + ' ' + '; Tel: ' + mobileTel.value;
+        var botLink = 'https://api.telegram.org/bot892587873:AAFw-auKXmBAvQ0x77e-hty-BvS-faOHS0Y/sendMessage?chat_id=214831481&text=' + msg;
+        fetch(botLink);
+        clearInputs();
+    } else {
+        checkInputs()
+    }
 })
-// else if (!firstName.value && !mobileTel.value) {
-//     firstName.style = "background: #ff00001f; border: 2px solid #620808";
-//     mobileTel.style = "background: #ff00001f; border: 2px solid #620808";
-// } else if (!firstName.value) {
-//     firstName.style = "background: #ff00001f; border: 2px solid #620808";
-// } else {
-//     mobileTel.style = "background: #ff00001f; border: 2px solid #620808";
-// }
+
+function clearInputs() {
+    var inputs = document.querySelectorAll('input');
+    for (i = 0; i < inputs.length; i++) {
+        inputs[i].value = '';
+    }
+}
+
+function checkInputs() {
+    var inputs = document.querySelectorAll('input')
+    for (i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            inputs[i].classList.add('empty');
+        } else {
+            continue;
+        }
+    }
+}
+function deleteError(){
+    var inputs = document.querySelectorAll('input')
+    for (i = 0; i < inputs.length; i++) {
+        if (inputs[i].classList.contains('empty')) {
+            inputs[i].classList.remove('empty');
+        } else {
+            continue;
+        }
+    }
+}

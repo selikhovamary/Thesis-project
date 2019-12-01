2a41c6eb45b5211b2ad6ff3ce1af5484b05be58e
selikhovamary <maryiaselikhova@gmail.com>
2019-12-01 15:16:49 +0300

    check sendMsg on the page

--- src/js/bot.js
+++ src/js/bot.js
@@ -2,26 +2,28 @@ var sendMsg = document.querySelector('#sendMsg');
 var firstName = document.querySelector('#name');
 var mobileTel = document.querySelector('#tel');
 var msg;
+if (document.contains(sendMsg)) {
+    sendMsg.addEventListener('click', function () {
+        deleteError()
+        if (firstName.value && mobileTel.value) {
+            msg = 'Name: ' + firstName.value + ' ' + '; Tel: ' + mobileTel.value;
+            var botLink = 'https://api.telegram.org/bot892587873:AAFw-auKXmBAvQ0x77e-hty-BvS-faOHS0Y/sendMessage?chat_id=214831481&text=' + msg;
+            fetch(botLink);
+            clearInputs();
+            sendMsg.classList.add('success');
+            sendMsg.innerHTML = '&#10004;';
+            setTimeout(function () {
+                if (sendMsg.classList.contains('success')) {
+                    sendMsg.classList.remove('success');
+                    sendMsg.innerHTML = 'Send';
+                }
+            }, 2000)
+        } else {
+            checkInputs()
+        }
+    })
+}
 
-sendMsg.addEventListener('click', function () {
-    deleteError()
-    if (firstName.value && mobileTel.value) {
-        msg = 'Name: ' + firstName.value + ' ' + '; Tel: ' + mobileTel.value;
-        var botLink = 'https://api.telegram.org/bot892587873:AAFw-auKXmBAvQ0x77e-hty-BvS-faOHS0Y/sendMessage?chat_id=214831481&text=' + msg;
-        fetch(botLink);
-        clearInputs();
-        sendMsg.classList.add('success');
-        sendMsg.innerHTML = '&#10004;';
-        setTimeout(function(){
-            if (sendMsg.classList.contains('success')){
-                sendMsg.classList.remove('success');
-                sendMsg.innerHTML = 'Send';
-            }
-        }, 2000)
-    } else {
-        checkInputs()
-    }
-})
 
 function clearInputs() {
     var inputs = document.querySelectorAll('input');
@@ -40,7 +42,8 @@ function checkInputs() {
         }
     }
 }
-function deleteError(){
+
+function deleteError() {
     var inputs = document.querySelectorAll('input')
     for (i = 0; i < inputs.length; i++) {
         if (inputs[i].classList.contains('empty')) {
@@ -50,4 +53,3 @@ function deleteError(){
         }
     }
 }
-
